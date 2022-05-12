1dffdcd8837885bc36276ceace39071774f7fe92
SUYAIB AL SIFAT <suyaib@SUYAIBs-MacBook-Air.local>
2022-05-13 03:32:25 +0600

    stripe configs bugs fixed

--- App.js
+++ App.js
@@ -15,6 +15,7 @@ const globalErrorHandler = require('./contoroller/errorController');
 const AppError = require('./utilits/appError');
 const userRouter = require('./routes/userRouters');
 const viewRouters = require('./routes/viewRouters');
+const bookingController = require('./contoroller/bookingController');
 const bookingRoutes = require('./routes/bookingRoutes');
 
 //const start = Date.now();
@@ -52,6 +53,13 @@ const limiter = rateLimit({
   message: 'Too many requests from this IP, please try again in an hour!'
 });
 app.use('/api', limiter);
+
+// Stripe webhook, BEFORE body-parser, because stripe needs the body as stream
+app.post(
+  '/webhook-checkout',
+  express.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout
+);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '100kb' }));
